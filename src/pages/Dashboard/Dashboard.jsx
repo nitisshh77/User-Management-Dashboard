c9b1b0ea22d49d86a09dfd91067359e334ea3427
Nitish Bhardwaj <nitisshhhh@gmail.com>
2025-10-25 14:30:17 +0530

    Dashboard commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
 import DashboardContent from './DashboardContent'
-import { Brightness4, Brightness7 } from "@mui/icons-material";
-import { ColorModeContext } from "../../context/ThemeContext";
 
 
 const Dashboard = () => {
